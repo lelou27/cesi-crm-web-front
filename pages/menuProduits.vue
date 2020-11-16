34d0365acc8367ab6d1f1762e4087d22943c08d7
Antoine Henrio <antoine.henrio@orange.fr>
2020-11-16 16:23:30 +0100

    Menu des produits

---
+++ pages/menuProduits.vue
@@ -0,0 +1,57 @@
+<style>
+.conteneurBoutons {
+	display: grid;
+	grid-template-rows: 1fr 1fr 1fr;
+	grid-row-gap: 2vh;
+	justify-content: center;
+}
+
+.btnMain {
+	width: 33vw;
+	margin: auto;
+	height: 10vh;
+	background-color: #363636;
+	color: white;
+	font-size: 28px;
+	border: none;
+}
+
+.mainTitle {
+	font-size: 28px;
+	font-weight: bold;
+	text-align: center;
+	margin-top: 5vh;
+}
+
+.wrapper {
+	display: grid;
+	grid-template-rows: 1fr 1fr 1fr;
+}
+
+.exit {
+	font-size: 40px !important;
+}
+</style>
+
+<template>
+	<div>
+		<head>
+			<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+		</head>
+		<div class="wrapper">
+			<div class="mainTitle">
+				<h1>
+					Menu des produits
+				</h1>
+			</div>
+			<div class="conteneurBoutons">
+					<button class="btnMain">Gestion des composants</button>
+					<button class="btnMain">Gestion des modules</button>
+					<button class="btnMain">Gestion des gammes</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
