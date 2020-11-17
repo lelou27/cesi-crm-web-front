de64ec2faec8d359b9d3631b0f692e2478b7dc59
Antoine Henrio <antoine.henrio@orange.fr>
2020-11-17 09:31:07 +0100

    erreurs

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,10 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }, 
+            { rel: "stylesheet", href: "http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"},
+          ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -32,7 +35,6 @@ export default {
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
   ],
-
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
